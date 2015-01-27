cb683590a76dd0ddbdf0ac7bcd49d42b72908ea4
aadebuger <admin@local.host>
2015-01-27 14:24:44 +0800

    qiniu upgrade

--- myqiniupload.py
+++ myqiniupload.py
@@ -5,26 +5,31 @@ Created on 2014年3月5日
 @author: aadebuger
 '''
 
-import qiniu.io
-import qiniu.rs
-import qiniu.conf
+#import qiniu.io
+#import qiniu.rs
+#import qiniu.conf 
+import qiniu
+from qiniu import Auth, set_default, etag, PersistentFop, build_op, op_save
+
 import sys
 
 import os
 def token(bucket_name):
     print os.getenv("accesskey")
     print os.getenv("secretkey")
-    qiniu.conf.ACCESS_KEY = os.getenv("accesskey")
-    qiniu.conf.SECRET_KEY = os.getenv("secretkey")
-    policy = qiniu.rs.PutPolicy(bucket_name)
-    uptoken = policy.token()
-    return uptoken
+ #   qiniu.conf.ACCESS_KEY = os.getenv("accesskey")
+ #   qiniu.conf.SECRET_KEY = os.getenv("secretkey")
+ #   policy = qiniu.rs.PutPolicy(bucket_name)
+ #   uptoken = policy.token()
+    newauth = Auth(os.getenv("accesskey"), os.getenv("secretkey"))
+
+    return newauth.upload_token(bucket_name);
 
 def tokenext(bucket_name,accesskey,secretkey):
     qiniu.conf.ACCESS_KEY = accesskey
     qiniu.conf.SECRET_KEY = secretkey
     policy = qiniu.rs.PutPolicy(bucket_name)
-    uptoken = policy.token()
+    uptoken = policy.upload_token()
     return uptoken
 
 def cloudupload(uptoken,key,localfile):
@@ -32,7 +37,7 @@ def cloudupload(uptoken,key,localfile):
 
 #    localfile = "%s" % __file__
 
-    ret, err = qiniu.io.put_file(uptoken, key, localfile)
+    ret, err = qiniu.put_file(uptoken, key, localfile)
     if err is not None:
         sys.stderr.write('error: %s ' % err)
     print 'ret=',ret
